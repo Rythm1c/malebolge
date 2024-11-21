7ff7ac3929dfdb8a0fb1187bb3b39cf2207b4cb2
victor <88584256+Rythm1c@users.noreply.github.com>
2024-11-21 18:08:32 +0300

    Add files via upload

---
+++ animation/clip.h
@@ -0,0 +1,37 @@
+#ifndef CLIP_H
+#define CLIP_H
+
+#include <iostream>
+#include <vector>
+
+class Clip
+{
+public:
+    Clip();
+    ~Clip() {}
+
+    uint getIdAtIndex(uint index);
+    void seyIdAtIndex(uint idx, uint id);
+    uint size();
+    float sample(class Pose &outPose, float inTime);
+    void ReCalculateDuartion();
+
+    std::string &GetName();
+    void SetName(const std::string &inNewName);
+    float GetDuration();
+    float GetStartTime();
+    float GetEndTime();
+    bool GetLooping();
+    void SetLooping(bool inLooping);
+
+private:
+    std::string name;
+    float startTime;
+    float endTime;
+    bool looping;
+    std::vector<class TransformTrack> tracks;
+
+    float adjustTimeToFitRange(float time);
+};
+
+#endif
